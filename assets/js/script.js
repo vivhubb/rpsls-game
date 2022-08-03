7e9c5797c62ba862180a8c2eb1ad512afd84b5d8
vivienne <vivienne.2214@yahoo.com>
2022-08-03 11:02:08 +0000

    add event listener for loading DOM before running game

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// wair for the DOM to load content before running game
+// get buttons and add event listeneres to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName('button');
+
+    for (let button of buttons) {
+        button.addEventListener('click', function(){
+
+            let playerChoice = this.getAttribute('data-type');
+
+            game(playerChoice);
+        })
+    }
+} );
+
+
+
+
+
