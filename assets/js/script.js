c7bee45353e0983eaed186a1f1342a4ea7c024cc
vivienne <vivienne.2214@yahoo.com>
2022-08-03 13:03:01 +0000

    add global variables for choices and random selection for computer choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,35 @@
+// global variables for available choices
+let choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+let winners = {
+    rock: {
+        scissors: 'ROCK crushes SCISSORS',
+        lizard: 'ROCK crushes LIZARD'
+    },
+
+    paper: {
+        rock: 'PAPER covers ROCK',
+        spock: 'PAPER disproves SPOCK'
+    },
+
+    scissors: {
+        paper: 'SCISSORS cuts PAPER',
+        lizard: 'SCISSORS decapitates LIZARD'
+    },
+
+    lizard: {
+        spock: 'LIZARD poisons SPOCK',
+        paper: 'LIZARD eats PAPER'
+    },
+
+    spock: {
+        scissors: 'SPOCK smashes SCISSORS',
+        rock: 'SPOCK vaporizes ROCK'
+    }
+};
+ 
+
 // wair for the DOM to load content before running game
-// get buttons and add event listeneres to them
+// get buttons and add event listeners to them
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName('button');
 
@@ -22,7 +52,12 @@ function game(playerChoice) {
     resetZones();
 
     displayPlayerChoice(playerChoice);
-};
+
+    //  creates random choice for the computer
+    let randomNumber = Math.floor(Math.random() * 5); 
+    let computerChoice = choices[randomNumber];   
+    
+}
 
 /**
  * this function displays the choice made by the player in the div
@@ -40,7 +75,7 @@ function displayPlayerChoice(playerChoice) {
 
     playerZone.appendChild(displayChoice);
 
-};
+}
 
 /**
  * this function deletes the previously selected images
@@ -52,4 +87,6 @@ function resetZones() {
     for (let i = 0; i < replaceImage.length; i++) {
         replaceImage[i].remove();
     }
-};
+
+}
+
